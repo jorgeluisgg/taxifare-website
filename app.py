26aaccdc449842b42d85dd45e148263ce546d71d
JORGE LUIS GARCIA GABILONDO <gabilondo.jlu@gmail.com>
2024-05-24 07:24:29 -0600

    My first website

---
+++ app.py
@@ -0,0 +1,77 @@
+import streamlit as st
+import requests
+import datetime
+
+'''
+# TaxiFareModel front
+'''
+
+st.markdown('''
+Remember that there are several ways to output content into your web page...
+
+Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
+''')
+
+'''
+## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
+
+1. Let's ask for:
+- date and time
+- pickup longitude
+- pickup latitude
+- dropoff longitude
+- dropoff latitude
+- passenger count
+'''
+
+'''
+## Once we have these, let's call our API in order to retrieve a prediction
+
+See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
+
+🤔 How could we call our API ? Off course... The `requests` package 💡
+'''
+
+
+
+st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
+
+'''
+
+2. Let's build a dictionary containing the parameters for our API...
+
+3. Let's call our API using the `requests` package...
+
+4. Let's retrieve the prediction from the **JSON** returned by the API...
+
+## Finally, we can display the prediction to the user
+'''
+
+#1.
+date = st.date_input("Pickup date")
+time = st.time_input("Pickup time")
+pickup_longitude = st.number_input('Pickup longitude')
+pickup_latitude = st.number_input('Pickup latitute')
+dropoff_longitude = st.number_input('Dropoff longitude')
+dropoff_latitude = st.number_input('Dropoff latitude')
+passenger_count = st.number_input('Passenger count', min_value=1, max_value=8)
+pickup_datetime = f"{date} {time}"
+
+#2.
+params = {
+    'pickup_datetime': pickup_datetime,
+    'pickup_longitude': pickup_longitude,
+    'pickup_latitude': pickup_latitude,
+    'dropoff_longitude': dropoff_longitude,
+    'dropoff_latitude': dropoff_latitude,
+    'passenger_count': passenger_count
+}
+
+#3.
+if st.button('Get Fare Prediction'):
+    url = 'https://taxifare.lewagon.ai/predict'
+    request = requests.get(url,params = params)
+    prediction = request.json()
+#4.
+    st.header('Prediction')
+    st.write(f"The predicted fare is: ${prediction['fare']}")
